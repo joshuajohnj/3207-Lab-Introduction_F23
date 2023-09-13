b5da6560e6e196b6669b3a00a4d0c778ae001191
Joshua John <joshuajohn@Joshuas-Air.lan>
2023-09-13 04:08:46 -0400

    tuls.c: recursive function is fixed, listFile() and directory checking is finished, directory added for checking tuls

---
+++ tuls.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+enum result {
+    SUCCESS,
+    FAILURE
+} result;
+
+void listFiles()
+{
+    struct dirent **namelist;
+    int n;
+
+    n = scandir(".", &namelist, NULL, alphasort);
+    if (n < 0)
+        perror("tuls: cannot open directory");
+        printf("tuls error: %s", strerror(errno));
+    for(int i= 0; i<n; i++)
+    {
+        printf("%s\n", namelist[i]->d_name);
+        free(namelist[i]);
+    }
+    
+}
+int isDirectory(char * directoryName)
+{
+    struct stat check;
+    stat(directoryName,&check);
+
+    if(S_ISDIR(check.st_mode) != 0)
+    {
+        return SUCCESS;
+    }
+    return FAILURE;
+    
+}
+
+// function to print Directory recursively
+int recursivelyPrint(char* directoryName, int tabCount)
+{
+    
+    // open directory with param
+    DIR *dir = opendir(directoryName);
+    if(dir == NULL)
+    {
+        printf("opendir error: %s \n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    // init dirent struct
+    struct dirent *entry;
+    // create a previous directory path, to be used later
+    DIR * prevdir;
+ 
+    // iterate through out each entry in the directory
+    while((entry = readdir(dir)) != NULL)
+    {
+        // if were in the recurisive function print a tab for each file
+        if(tabCount == 1  && !(isDirectory(entry->d_name)))
+        {
+            printf("   ");
+        }
+        //else if were in the intial directory print without tab
+        printf("%s\n", entry->d_name);
+
+        if(isDirectory(entry->d_name) && strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0 )
+        {
+        /*
+            we've found a directory!
+            now construct the path and call the function recursively
+        */
+            char path[1024] ="";
+            strcat(path,directoryName);
+            strcat(path,"/");
+            strcat(path,entry->d_name);
+            recursivelyPrint(path,1);
+        }
+        // go back to the intial directory and read it 
+        prevdir = opendir("..");
+        entry = readdir(prevdir);
+    }
+
+    closedir(dir);
+    closedir(prevdir);
+    return SUCCESS;
+}
+//main
+
+int main(int argc, char*argv[])
+{
+    // if there is more than one params exit code
+    if(argc > 2)
+    {
+        printf("tuls: too many arguments\nError: %s", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    // if there is no params
+    if(argc == 1) {listFiles(); exit(EXIT_SUCCESS);}
+
+    // if there is 2 commands
+    else {recursivelyPrint(argv[1],0); exit(EXIT_SUCCESS);}
+}
